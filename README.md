9315d7ec31738fd7bf29282de128514492eef9ef
Matt Layher <mdlayher@gmail.com>
2017-10-20 23:46:41 -0400

    *: updated indexing script, now produces talks.json

--- README.md
+++ README.md
@@ -1,12 +1,14 @@
 talks [![Build Status](https://travis-ci.org/mdlayher/talks.svg?branch=master)](https://travis-ci.org/mdlayher/talks)
 =====
 
-Talks by Matt Layher using the Go present tool.  MIT Licensed.
+Talks by Matt Layher.  MIT Licensed.
 
 Talks
 -----
-- [2016 Sep 28 - Self-contained testing in Go](http://go-talks.appspot.com/github.com/mdlayher/talks/self-contained-testing-in-go.slide)
-- [2016 Sep 30 - package ovs](http://go-talks.appspot.com/github.com/mdlayher/talks/package-ovs.slide)
-- [2017 Mar 03 - A brief intro to Go](http://go-talks.appspot.com/github.com/mdlayher/talks/a-brief-intro-to-go.slide)
-- [2017 Apr 19 - Open sourcing code at DigitalOcean](http://go-talks.appspot.com/github.com/mdlayher/talks/open-sourcing-code-at-digitalocean.slide)
-- [2017 Jul 15 - Ethernet and Go](http://go-talks.appspot.com/github.com/mdlayher/talks/ethernet-and-go.slide)
+
+- [GopherCon 2017 - Lightning Talk: Ethernet and Go](https://www.youtube.com/watch?v=DgNiktCFuBg) [[slides](http://go-talks.appspot.com/github.com/mdlayher/talks/ethernet-and-go.slide#1)]
+  - A lightning talk about using Ethernet frames and raw sockets directly in Go.
+- Open sourcing code at DigitalOcean [[slides](http://go-talks.appspot.com/github.com/mdlayher/talks/open-sourcing-code-at-digitalocean.slide)]
+- A brief intro to Go [[slides](http://go-talks.appspot.com/github.com/mdlayher/talks/a-brief-intro-to-go.slide)]
+- package ovs [[slides](http://go-talks.appspot.com/github.com/mdlayher/talks/package-ovs.slide)]
+- Self-contained testing in Go [[slides](http://go-talks.appspot.com/github.com/mdlayher/talks/self-contained-testing-in-go.slide)]
--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"html/template"
 	"log"
 	"net/url"
@@ -13,11 +14,16 @@ import (
 	"golang.org/x/tools/present"
 )
 
-//go:generate bash -c "go run main.go > README.md"
+//go:generate go run main.go
 
-// prefix is the base URL for the go-talks website with this repo's
-// name prefixed.
-const prefix = "http://go-talks.appspot.com/github.com/mdlayher/talks/"
+const (
+	// prefix is the base URL for the go-talks website with this repo's
+	// name prefixed.
+	prefix = "http://go-talks.appspot.com/github.com/mdlayher/talks/"
+
+	// talksJSON is the name of the JSON metadata file produced by this script.
+	talksJSON = "talks.json"
+)
 
 func main() {
 	base, err := url.Parse(prefix)
@@ -26,78 +32,140 @@ func main() {
 	}
 
 	// Look for all presentations in '.slide' format.
-	var ps []presentation
+	var ps []*presentation
 	err = filepath.Walk(".", func(path string, info os.FileInfo, err error) error {
 		if err != nil {
 			return err
 		}
 
-		if info.IsDir() || filepath.Ext(path) != ".slide" {
+		if info.IsDir() {
 			return nil
 		}
 
-		f, err := os.Open(path)
+		var fn func(path string, base *url.URL) (*presentation, error)
+		switch filepath.Ext(path) {
+		case ".json":
+			// Skip special metadata file.
+			if path == talksJSON {
+				return nil
+			}
+
+			fn = parseJSON
+		case ".slide":
+			// Don't parse slides if a JSON file exists instead.
+			noExt := strings.TrimSuffix(path, filepath.Ext(path))
+			if _, err := os.Stat(noExt + ".json"); err == nil {
+				return nil
+			}
+
+			fn = parsePresentation
+		default:
+			return nil
+		}
+
+		p, err := fn(path, base)
 		if err != nil {
 			return err
 		}
-		defer f.Close()
 
-		// Parse a presentation to retrieve its metadata
-		doc, err := present.Parse(f, path, 0)
-		if err != nil {
-			return err
-		}
-
-		u, err := url.Parse(path)
-		if err != nil {
-			return err
-		}
-
-		// Build a presentation to be output as part of the template
-		ps = append(ps, presentation{
-			Title:    doc.Title,
-			Subtitle: doc.Subtitle,
-			Date:     doc.Time.Format("2006 Jan 02"),
-			Time:     doc.Time,
-			URL:      base.ResolveReference(u).String(),
-		})
+		ps = append(ps, p)
 		return nil
 	})
 	if err != nil {
 		log.Fatalf("unexpected error during filesystem walk: %v", err)
 	}
 
-	// Order all presentations by their date and time, and then output the template
-	sort.Sort(byTime(ps))
-	_ = markdown.Execute(os.Stdout, ps)
+	// Order all presentations by latest date and time, and then output the template.
+	sort.Slice(ps, func(i int, j int) bool {
+		return ps[i].Time.After(ps[j].Time)
+	})
+
+	// Generate README.md.
+	readme, err := os.Create("README.md")
+	if err != nil {
+		log.Fatalf("failed to create README: %v", err)
+	}
+	defer readme.Close()
+
+	if err := markdown.Execute(readme, ps); err != nil {
+		log.Fatalf("failed to execute template: %v", err)
+	}
+
+	// Generate talks.json metadata.
+	talks, err := os.Create(talksJSON)
+	if err != nil {
+		log.Fatalf("failed to create talks.json: %v", err)
+	}
+	defer talks.Close()
+
+	if err := json.NewEncoder(talks).Encode(ps); err != nil {
+		log.Fatalf("failed to encode JSON: %v", err)
+	}
 }
 
-// byTime sorts a slice of presentations by their Time field.
-type byTime []presentation
+func parseJSON(path string, base *url.URL) (*presentation, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
 
-func (t byTime) Len() int           { return len(t) }
-func (t byTime) Less(i, j int) bool { return t[i].Time.Before(t[j].Time) }
-func (t byTime) Swap(i, j int)      { t[i], t[j] = t[j], t[i] }
+	var p presentation
+	if err := json.NewDecoder(f).Decode(&p); err != nil {
+		return nil, err
+	}
 
-// A presentation is a present-format presentation.
+	return &p, nil
+}
+
+func parsePresentation(path string, base *url.URL) (*presentation, error) {
+	f, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	// Parse a presentation to retrieve its metadata.
+	doc, err := present.Parse(f, path, 0)
+	if err != nil {
+		return nil, err
+	}
+
+	u, err := url.Parse(path)
+	if err != nil {
+		return nil, err
+	}
+
+	// Build a presentation to be output as part of the template.
+	return &presentation{
+		Title:       doc.Title,
+		Description: doc.Subtitle,
+		Time:        doc.Time,
+		SlidesLink:  base.ResolveReference(u).String(),
+	}, nil
+}
+
+// A presentation is a presentation's metadata.
 type presentation struct {
-	Title    string
-	Subtitle string
-	Time     time.Time
-	Date     string
-	URL      string
+	Title       string
+	Description string
+	Time        time.Time
+	SlidesLink  string
+	VideoLink   string
 }
 
-// markdown is the markdown template for README.md
+// markdown is the markdown template for README.md.
 var markdown = template.Must(template.New("README.md").Parse(strings.TrimSpace(`
 talks [![Build Status](https://travis-ci.org/mdlayher/talks.svg?branch=master)](https://travis-ci.org/mdlayher/talks)
 =====
 
-Talks by Matt Layher using the Go present tool.  MIT Licensed.
+Talks by Matt Layher. MIT Licensed.
 
 Talks
 -----
-{{range .}}- [{{.Date}} - {{.Title}}]({{.URL}}){{if .Subtitle}}
-  - {{.Subtitle}}{{end}}
-{{end}}
+{{range .}}{{if .VideoLink}}
+- [{{.Title}}]({{.VideoLink}}) [[slides]({{.SlidesLink}})]{{if .Description}}
+  - {{.Description}}{{end}}{{else}}
+- {{.Title}} [[slides]({{.SlidesLink}})]{{if .Description}}
+  - {{.Description}}{{end}}{{end}}{{end}}
 `)))
